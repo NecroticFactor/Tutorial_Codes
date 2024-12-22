1fbe76c418985c3d824bd5dd5323f91bca356b3d
Dr.A07 <157939154+NecroticFactor@users.noreply.github.com>
2024-12-22 15:49:35 +0530

    Initial Commit
    Working on the Backend using Django

---
+++ Frontend & Backend/Backend/backend/api/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import *
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import *
+
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+
+class NoteListView(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        note = Note.objects.filter(author=user)
+        return note
+
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
